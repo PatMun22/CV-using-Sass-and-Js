56a393845c36192ad0d904d5058d2a74834978b9
Patrick Munuku <wafulapatrick6@gmail.com>
2023-08-14 21:41:55 +0300

    Additional functionality/dark mode modification

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up');
-    this.scrollY >= 150 ? scrollUp.classList.add('show-scroll')
+    this.scrollY >=  50 ? scrollUp.classList.add('show-scroll')
                     : scrollUp.classList.remove('show-scroll');
 }
 
 window.addEventListener('scroll', scrollUp);
 
 /* ---------------------theme change --------------*/
-
+/*
 const themeButton = document.querySelector('#theme-button');
 const darkTheme = 'dark-theme';
-const iconTheme = 'fa-lightbulb-on';
+const iconTheme = 'fa-sharp fa-solid fa-sun-bright';
 
 //previously selected topic(if user selected)
 
@@ -22,14 +22,14 @@ const selectedIcon = localStorage.getItem('selected-icon');
 //Validating dark-theme class to obtain current theme
 
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : ' ';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-solid fa-moon' : 'fa-sharp fa-solid fa-sun-bright';
 
 //validating if user previously chose a topic
 
 if(selectedTheme) {
     //if the validation is fulfilled, we ask what the issue was to know if we activated or deactivated
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeButton.classList[selectedIcon === 'fa-lightbulb-on' ? 'add' : 'remove'](iconTheme);  
+    themeButton.classList[selectedIcon === 'fa-solid fa-moon' ? 'add' : 'remove'](iconTheme);  
 }
 
 //Manually activating/deactivating theme with the button
@@ -39,5 +39,21 @@ themeButton.addEventListener('click', () => {
     themeButton.classList.toggle(iconTheme);
     //saving theme and current icon the user chose
     localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selectedIcon', getCurrentIon());
+    localStorage.setItem('selectedIcon', getCurrentIcon()); 
+})
+
+*/
+
+let icon = document.getElementById('theme-button');
+
+icon.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    if(body.classList.contains('dark-theme')) {
+        icon.classList.contains('fa-solid fa-moon');
+        icon.classList.contains('fa-sharp fa-solid fa-sun-bright');
+    } else {
+        icon.classList.contains('fa-sharp fa-solid fa-sun-bright');
+        icon.classList.contains('fa-solid fa-moon');
+    }
 })
